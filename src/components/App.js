491afba01d61af64ccc1fedbf379f0e235858a42
Kuddleman <edl1155@gmail.com>
2020-12-29 18:58:54 -0800

    initial set-up of app

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'  
+import SearchBar from './SearchBar'
+
+const App = () => {
+    return <div>
+                Hello from App
+                <SearchBar />
+            </div>
+}
+
+export default App
