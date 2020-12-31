976523fc1eb7c43dd1bc4791544fd08498fbfd14
Kuddleman <edl1155@gmail.com>
2020-12-30 20:08:37 -0800

    add form functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react'  
 import SearchBar from './SearchBar'
 
-const App = () => {
-    return <div>
-                Hello from App
-                <SearchBar />
-            </div>
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term)
+    }
+        
+    render() {
+        return (
+                <div className="ui container">
+                    <SearchBar onSubmit={this.onSearchSubmit}/>
+                </div>
+        )    
+    }
 }
 
 export default App
