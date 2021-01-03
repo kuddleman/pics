0a62cce8910625d7a46ef32d762bd56b89063ad6
Kuddleman <edl1155@gmail.com>
2021-01-02 19:27:34 -0800

    add axios async data request

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from 'react'  
+import React from 'react' 
+import axios from 'axios' 
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term)
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 24y5Z4vaLfMz5w56aFQCUFjBpJOF7-NzT8MM2ysY0Ow'
+
+            }
+        })
+        console.log(response.data.results)
     }
         
     render() {
