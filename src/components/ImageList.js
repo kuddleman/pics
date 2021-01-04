b3de05d5057aefa67f7a6771a140dc82eec7dbc9
Kuddleman <edl1155@gmail.com>
2021-01-03 19:03:18 -0800

    render list in ImageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,13 @@
 import React from 'react' 
 
 const ImageList = (props) => {
-    console.log(props.images)
+    
+    const images = props.images.map(({description, id, urls}) => {
+        return <img alt={description} key={id} src={urls.regular}  />
+    })
     return (
             <div>
-                Hello from ImageList
+               {images}
             </div>
     )
 }
