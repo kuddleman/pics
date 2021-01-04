04df72c0bd100fe9a79c614625fc4af41981372e
Kuddleman <edl1155@gmail.com>
2021-01-03 18:22:59 -0800

    add ImageList component

---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+const ImageList = (props) => {
+    console.log(props.images)
+    return (
+            <div>
+                Hello from ImageList
+            </div>
+    )
+}
+
+export default ImageList
